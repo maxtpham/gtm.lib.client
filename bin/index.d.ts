1af11c1b6cc236eec98c1d47d764fa17705c02bc
Thanh Pham <thanhptr@gmail.com>
2018-01-29 23:19:58 +0700

    Move index -> api

--- bin/index.d.ts
+++ bin/index.d.ts
@@ -1,13 +1,2 @@
-import { interfaces } from "inversify";
-export interface ApiResponse<T> {
-    response: Response;
-    body?: T;
-}
-export declare abstract class ApiClient {
-    protected _basePath: string;
-    protected accessToken: string;
-    constructor(basePath?: string, accessToken?: string);
-    protected readonly defaultHeaders: {};
-    execute<T>(method: string, path: string, queryParameters: {}, headerParams: {}, formParams: {}, isFile: boolean, isResponseFile: boolean, bodyParam?: string, ...authMethods: string[]): Promise<ApiResponse<T>>;
-}
-export declare function registerApiClient<T extends ApiClient>(iocContainer: interfaces.Container, serviceIdentifier: interfaces.ServiceIdentifier<T>, ctor: new (basePath?: string, accessToken?: string) => T, basePath: string, token?: string | (() => string)): void;
+export * from './api';
+export * from './auth';
--- bin/index.js
+++ bin/index.js
@@ -1,71 +1,8 @@
 "use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
 Object.defineProperty(exports, "__esModule", { value: true });
-class ApiClient {
-    constructor(basePath, accessToken) {
-        this._basePath = 'http://localhost';
-        if (!!basePath) {
-            this._basePath = basePath;
-        }
-        if (!!accessToken) {
-            this.accessToken = accessToken;
-        }
-    }
-    get defaultHeaders() {
-        return {};
-    }
-    execute(method, path, queryParameters, headerParams, formParams, isFile, isResponseFile, bodyParam, ...authMethods) {
-        const requestOptions = {
-            method: method,
-            headers: headerParams
-        };
-        if (bodyParam) {
-            requestOptions.body = JSON.parse(bodyParam);
-        }
-        if (this.accessToken && requestOptions && requestOptions.headers) {
-            requestOptions.headers["Authorization"] = "Bearer " + this.accessToken;
-        }
-        return new Promise((resolve, reject) => {
-            fetch(this._basePath + path, requestOptions)
-                .catch(e => reject(e))
-                .then(response => {
-                if (!!response && response.status >= 200 && response.status < 300) {
-                    response.json()
-                        .catch(e => reject({ response: response, body: e }))
-                        .then(body => resolve({ response: response, body: body }));
-                }
-                else {
-                    reject({ response: response });
-                }
-            });
-        });
-    }
-}
-exports.ApiClient = ApiClient;
-function registerApiClient(iocContainer, serviceIdentifier, ctor, basePath, token) {
-    if (!!iocContainer.bind(serviceIdentifier)) {
-        iocContainer.rebind(serviceIdentifier).toDynamicValue(new ApiClientFactory(ctor, basePath, token).handler);
-    }
-    else {
-        iocContainer.bind(serviceIdentifier).toDynamicValue(new ApiClientFactory(ctor, basePath, token).handler);
-    }
-}
-exports.registerApiClient = registerApiClient;
-class ApiClientFactory {
-    constructor(ctor, basePath, token) {
-        this.ctor = ctor;
-        this.basePath = basePath;
-        if (!!token) {
-            this.token = token;
-        }
-    }
-    createWithGetter() {
-        return new this.ctor(this.basePath, this.token());
-    }
-    createWithValue() {
-        return new this.ctor(this.basePath, this.token);
-    }
-    get handler() {
-        return typeof (this.token) === 'function' ? this.createWithGetter.bind(this) : this.createWithValue.bind(this);
-    }
-}
+__export(require("./api"));
+__export(require("./auth"));
 //# sourceMappingURL=index.js.map
