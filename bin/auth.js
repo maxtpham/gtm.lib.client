76a3d77343a9b1d975236ac09c187b81f9fefd3a
Thanh Pham <thanhptr@gmail.com>
2018-01-29 23:13:15 +0700

    Added auth module

---
+++ bin/auth.js
@@ -0,0 +1,95 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var auth;
+(function (auth) {
+    function getLoginUrl(userServiceUrl, provider, applicationScheme) {
+        return `${userServiceUrl}/pub/auth/login/${provider}?callback=` +
+            encodeURIComponent(`${userServiceUrl}/web/auth/to?uri=` +
+                encodeURIComponent(`${applicationScheme}://login/${provider}?token={TOKEN}`));
+    }
+    auth.getLoginUrl = getLoginUrl;
+    function logout(dataRemover) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield dataRemover('auth.jwt');
+        });
+    }
+    auth.logout = logout;
+    function authenticate(userServiceUrl, initialUrlGetter, dataGetter, dataSetter, dataRemover) {
+        return __awaiter(this, void 0, void 0, function* () {
+            let initialUrlValue = yield dataGetter('auth.initialUrl.value');
+            let initialUrlProcessed = yield dataGetter('auth.initialUrl.processed');
+            const initialUrl = yield initialUrlGetter();
+            if (!!initialUrl && initialUrlValue !== initialUrl) {
+                yield dataSetter('auth.initialUrl.value', initialUrlValue = initialUrl);
+                yield dataSetter('auth.initialUrl.processed', initialUrlProcessed = '0');
+                console.log(`[Auth] Found new InitialUrl: ${initialUrl}`);
+            }
+            if (!!initialUrlValue && (!initialUrlProcessed || initialUrlProcessed == '0')) {
+                const jwt = yield handleNewInitialUrl(userServiceUrl, initialUrlValue);
+                yield dataSetter('auth.jwt', jwt);
+                yield dataSetter('auth.initialUrl.processed', '1');
+            }
+            const jwt = yield dataGetter('auth.jwt');
+            if (!!jwt && !isLoggedIn(userServiceUrl, jwt)) {
+                console.log(`[Auth] Current Jwt Token is timed out & will be cleared`);
+                yield dataRemover('auth.jwt');
+                return undefined;
+            }
+            return jwt;
+        });
+    }
+    auth.authenticate = authenticate;
+    function handleNewInitialUrl(userServiceUrl, initialUrlValue) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const pos = initialUrlValue.indexOf('?token=');
+            if (pos < 0) {
+                return Promise.reject('[Auth] Invalid OAuth token InitialUrl');
+            }
+            else {
+                let token = initialUrlValue.substr(pos + '?token='.length);
+                while (token.length > 0 && token.endsWith('#')) {
+                    token = token.substr(0, token.length - 1);
+                }
+                console.log(`[Auth] Exchanging OAuth token: ${token}`);
+                const jwt = yield exchangeToken(userServiceUrl, token);
+                console.log(`[Auth] New JWT token: ${jwt}`);
+                return jwt;
+            }
+        });
+    }
+    function exchangeToken(userServiceUrl, token) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch(`${userServiceUrl}/web/auth/jwt?t=${encodeURIComponent(token)}`);
+            if (!response.ok) {
+                return Promise.reject(`${response.status} - ${response.statusText}`);
+            }
+            else {
+                return response.text();
+            }
+        });
+    }
+    function isLoggedIn(userServiceUrl, jwt) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const response = yield fetch(`${userServiceUrl}/api/v1/system/loggedin`, {
+                headers: {
+                    'Authorization': 'Bearer ' + jwt,
+                }
+            });
+            if (!response.ok) {
+                return Promise.reject(`${response.status} - ${response.statusText}`);
+            }
+            else {
+                return response.json();
+            }
+        });
+    }
+})(auth = exports.auth || (exports.auth = {}));
+//# sourceMappingURL=auth.js.map
---
+++ src/auth.ts
@@ -0,0 +1,86 @@
+export module auth {
+
+    export type OAuth2Provider = 'google' | 'facebook';
+
+    export function getLoginUrl(userServiceUrl: string, provider: OAuth2Provider, applicationScheme: string) {
+        return `${userServiceUrl}/pub/auth/login/${provider}?callback=` +
+        encodeURIComponent(`${userServiceUrl}/web/auth/to?uri=` +
+            encodeURIComponent(`${applicationScheme}://login/${provider}?token={TOKEN}`)
+        )
+    }
+    
+    export async function logout(
+        dataRemover: (key: string) => Promise<void>
+    ): Promise<void> {
+        await dataRemover('auth.jwt');
+    }
+    
+    export async function authenticate(
+        userServiceUrl: string,
+        initialUrlGetter: () => Promise<string>,
+        dataGetter: (key: string) => Promise<string>,
+        dataSetter: (key: string, value: string) => Promise<void>,
+        dataRemover: (key: string) => Promise<void>
+    ): Promise<string> {
+        let initialUrlValue = await dataGetter('auth.initialUrl.value');
+        let initialUrlProcessed = await dataGetter('auth.initialUrl.processed');
+    
+        const initialUrl = await initialUrlGetter();
+        if (!!initialUrl && initialUrlValue !== initialUrl) {
+            await dataSetter('auth.initialUrl.value', initialUrlValue = initialUrl);
+            await dataSetter('auth.initialUrl.processed', initialUrlProcessed = '0');
+            console.log(`[Auth] Found new InitialUrl: ${initialUrl}`);
+        }
+        if (!!initialUrlValue && (!initialUrlProcessed || initialUrlProcessed == '0')) {
+            const jwt = await handleNewInitialUrl(userServiceUrl, initialUrlValue);
+            await dataSetter('auth.jwt', jwt);
+            await dataSetter('auth.initialUrl.processed', '1');
+        }
+    
+        const jwt = await dataGetter('auth.jwt');
+        if (!!jwt && !isLoggedIn(userServiceUrl, jwt)) {
+            console.log(`[Auth] Current Jwt Token is timed out & will be cleared`)
+            await dataRemover('auth.jwt');
+            return <any>undefined;
+        }
+        return jwt;
+    }
+    
+    async function handleNewInitialUrl(userServiceUrl: string, initialUrlValue: string): Promise<string> {
+        const pos = initialUrlValue.indexOf('?token=');
+        if (pos < 0) {
+            return Promise.reject('[Auth] Invalid OAuth token InitialUrl');
+        } else {
+            let token = initialUrlValue.substr(pos + '?token='.length);
+            while (token.length > 0 && token.endsWith('#')) {
+                token = token.substr(0, token.length - 1);
+            }
+            console.log(`[Auth] Exchanging OAuth token: ${token}`);
+            const jwt = await exchangeToken(userServiceUrl, token);
+            console.log(`[Auth] New JWT token: ${jwt}`);
+            return jwt;
+        }
+    }
+    
+    async function exchangeToken(userServiceUrl: string, token: string): Promise<string> {
+        const response = await fetch(`${userServiceUrl}/web/auth/jwt?t=${encodeURIComponent(token)}`);
+        if (!response.ok) {
+            return Promise.reject(`${response.status} - ${response.statusText}`);
+        } else {
+            return response.text();
+        }
+    }
+    
+    async function isLoggedIn(userServiceUrl: string, jwt: string): Promise<boolean> {
+        const response = await fetch(`${userServiceUrl}/api/v1/system/loggedin`, {
+            headers: {
+                'Authorization': 'Bearer ' + jwt,
+            }
+        });
+        if (!response.ok) {
+            return Promise.reject(`${response.status} - ${response.statusText}`);
+        } else {
+            return response.json();
+        }
+    }
+}
